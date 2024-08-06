483e00bfaf51cf3e91091098a9345a248c062fd5
Nikos Andriotis <67287051+andriotisnikos1@users.noreply.github.com>
2024-08-06 16:29:10 +0300

    c

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
 "use client"
 import { Link1Icon, Link2Icon } from "@radix-ui/react-icons";
 import Image from "next/image";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { toast, Toaster } from "sonner";
 import { scrapeCheck, scrapeURL } from "./actions";
+
+async function checkURL(f: FormData) {
+  // get url
+  const inputUrl = f.get("url")
+  if (!inputUrl) return toast.error("Please enter a URL");
+  // scrape check
+  toast.loading("Checking if scraping is allowed...");
+  const res = await scrapeCheck(inputUrl as string);
+  // result
+  if (res) toast.success("Scraping is allowed");
+  else toast.error("Scraping is not allowed");
+}
+
+async function scrape(f: FormData) {
+  // get url
+  const inputUrl = f.get("url");
+  if (!inputUrl) return toast.error("Please enter a URL");
+  // scrape
+  toast.loading(`Scraping %{${inputUrl}} ...`);
+  const res = await scrapeURL(inputUrl as string);
+  if (!res) return toast.error("Failed to scrape the URL");
+  // download
+  const blob = new Blob([res], { type: "text/plain" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = `scraped-${f.get("url")!}.html`
+  document.body.appendChild(a);
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
 
 export default function Home() {
   const [url, setUrl] = useState("");
@@ -16,33 +47,12 @@ export default function Home() {
           <input type="text" placeholder="Enter a URL....." className="outline-none bg-transparent w-4/5" onChange={(e) => setUrl(e.target.value)} />
         </div>
         <div className="flex items-center gap-2 px-2 flex-col md:flex-row ">
-          <form action={async (f) => {
-            const inputUrl = f.get("url")
-            if (!inputUrl) return toast.error("Please enter a URL");
-            toast.loading("Checking if scraping is allowed...");
-            const res = await scrapeCheck(inputUrl as string);
-            if (res) toast.success("Scraping is allowed");
-            else toast.error("Scraping is not allowed");
-          }}>
-            <input type="text" name="url" hidden readOnly value={url} required/>
+          <form action={checkURL}>
+            <input type="text" name="url" hidden readOnly value={url} required />
             <button type="submit" className="w-[250px] p-2 text-sm bg-slate-100 rounded-md ">Can i scrape that?</button>
           </form>
-          <form action={async (f) => {
-            const inputUrl = f.get("url");
-            if (!inputUrl) return toast.error("Please enter a URL");
-            toast.loading(`Scraping %{${inputUrl}} ...`);
-            const res = await scrapeURL(inputUrl as string);
-            if (!res) return toast.error("Failed to scrape the URL");
-            const blob = new Blob([res], { type: "text/plain" });
-            const url = URL.createObjectURL(blob);
-            const a = document.createElement("a");
-            a.href = url;
-            a.download = `scraped-${f.get("url")!}.html`
-            document.body.appendChild(a);
-            a.click();
-            window.URL.revokeObjectURL(url);
-          }}>
-            <input type="text" name="url" hidden readOnly value={url} required/>
+          <form action={scrape}>
+            <input type="text" name="url" hidden readOnly value={url} required />
             <button type="submit" className="w-[250px] p-2 text-sm bg-slate-100 rounded-md ">Scrape URL</button>
           </form>
         </div>
